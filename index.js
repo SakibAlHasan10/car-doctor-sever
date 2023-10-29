4d899eb29f2e6854d6cd9dbd1c7d34866bede353
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-29 10:15:24 +0600

    added delete cart items

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const app = express()
+const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 // MeddleWare
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.nwipcoy.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.nwipcoy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,35 +26,41 @@ async function run() {
     const carDoctor = client.db("carDoctor").collection("services");
     const bookings = client.db("carDoctor").collection("bookings");
     // get single services data
-    app.get(`/services/:id`, async(req, res)=>{
-        const id = req.params.id;
-        const filter = {_id: new ObjectId(id)}
-        const result = await carDoctor.findOne(filter)
-        res.send(result)
-    })
-    // get single services 
-    // app.get(`/booking`, async(req, res)=>{
-    //   let query = {}
-    //   if(req.query?.email){
-    //     qurey = {email: req.query?.email}
-    //   }
-    //   const result = await carDoctor.find(query).toArray()
-    //   res.send(result)
-    // })
+    app.get(`/services/:id`, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await carDoctor.findOne(filter);
+      res.send(result);
+    });
+
     // bookings
-    app.post(`/bookings`, async(req, res)=>{
+    app.get(`/bookings`, async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query?.email };
+      }
+      const result = await bookings.find(query).toArray();
+      res.send(result);
+    });
+    app.post(`/bookings`, async (req, res) => {
       const booking = req.body;
-      console.log(booking)
-      const result =  await bookings.insertOne(booking);
+      console.log(booking);
+      const result = await bookings.insertOne(booking);
+      res.send(result);
+    });
+    app.delete(`/bookings/:id`, async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result= await bookings.deleteOne(query)
       res.send(result)
+      // console.log(query)
     })
     // get all services data
-    app.get("/services", async(req, res)=>{
-        const cursor = carDoctor.find();
-        const result = await cursor.toArray()
-        res.send(result)
-    })
-
+    app.get("/services", async (req, res) => {
+      const cursor = carDoctor.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
